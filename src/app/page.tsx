81326a86c17fb8a7d1040f790ef340f21548d766
Ibrahimzene <131875836+Ibrahimzene@users.noreply.github.com>
2023-12-14 11:45:35 +0100

    Fix image and update social media links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
             Mahamat zene{''}<br/>Ibrahim
             
             {/* das ist meine logo oder  */}
-            <Image
+             <Image
               src="/images/logo.png"
               alt="mziy-Logo"
               className="dark:invert"
@@ -33,14 +33,15 @@ export default function Home() {
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
       <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/images/logo.png"
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert rounded-full w-60"
+          src="/images/Ibrahim.png"
           alt="mziy Logo"
           width={180}
           height={37}
           priority
         />
       </div>
+      
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
@@ -61,7 +62,7 @@ export default function Home() {
         </a>
 
         <a
-          href="https://www.linkedin.com/in/ibrahim-mahamat-zene-b39782279/"
+          href="https://www.linkedin.com/in/ibrahim-mahamat-zene/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -78,19 +79,19 @@ export default function Home() {
         </a>
 
         <a
-          href="https://twitter.com/mziy4"
+          href="https://www.xing.com/discover/updates"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            X{' '}
+            Xing{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Finde mich in X.
+            Finde mich in Xing.
           </p>
         </a>
 
