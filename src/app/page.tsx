d61cc22ec7b7b10ee0d2dca6d1fc0658ac4bbce5
Ibrahimzene <131875836+Ibrahimzene@users.noreply.github.com>
2023-12-14 17:30:57 +0100

    Update page content and fix formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,15 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Full-Stack&nbsp;
-          <code className="font-mono font-bold">Web Entwickler</code>
+        Ich bin im prozess&nbsp; 
+          <code className="font-mono font-bold">um <mark>Webentwickler</mark> zu werden.</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="#"
-            target="_blank"
-            rel="noopener noreferrer"
+            /* target="_blank"
+            rel="noopener noreferrer" */
           >
             Mahamat zene{""}
             <br />
@@ -103,6 +103,7 @@ export default function Home() {
           </p>
         </a>
       </div>
+
     </main>
   );
 }
